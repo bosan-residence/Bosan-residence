af255352209dfa955c0845dc7ea2849cc6f9fb5f
bosan-residence <7tcbk2cfds@privaterelay.appleid.com>
2026-09-07 16:19:30 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+
+const CACHE='bosan-pwa-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
